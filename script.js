3b146c44967a8a22958fa7689c5fc9c101e38c9c
Mikhail Menshikov <ainop@ya.ru>
2022-11-22 10:12:11 +0100

    remove useless async wrapper

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-const main = async () => {
-    const game = document.querySelector('.game');
+const game = document.querySelector('.game');
 
-    const play = document.querySelector('#play').content.firstElementChild.cloneNode(true);
-    const je_sh = document.querySelector('#je_sh').content.firstElementChild.cloneNode(true);
+const play = document.querySelector('#play').content.firstElementChild.cloneNode(true);
+const je_sh = document.querySelector('#je_sh').content.firstElementChild.cloneNode(true);
 
-    const audio = new Audio('bgsound.mp3');
-    audio.setAttribute('preload', 'auto');
-    audio.setAttribute('loop', true);
-    audio.load();
+const audio = new Audio('bgsound.mp3');
+audio.setAttribute('preload', 'auto');
+audio.setAttribute('loop', true);
+audio.load();
 
-    audio.addEventListener('canplaythrough', () => {
-        game.appendChild(play);
+audio.addEventListener('canplaythrough', () => {
+    game.appendChild(play);
 
-        play.addEventListener('click', () => {
-            play.remove();
-            game.appendChild(je_sh);
+    play.addEventListener('click', () => {
+        play.remove();
+        game.appendChild(je_sh);
 
-            audio.play();
-        });
-    }, { once: true });
-};
-
-main().then(() => {
-    console.log('ok');
-});
+        audio.play();
+    });
+}, { once: true });
